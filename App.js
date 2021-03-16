ee065a05e82b3d77bbe25b5be5040753cdac1c09
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2021-03-16 16:41:35 -0700

    testing authentication token

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Rolo');
-  const [age, setAge] = useState(30);
-  const nameChangeHandler = (value) => {
-    setName(value);
-  }
-  const ageChangeHandler = (value) => {
-    setAge(value);
-  }
+  
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text> and my age is {age}</Text>
-      <View>
-      <Text>My name is...</Text>
-       <TextInput
-       style={styles.inputContainer}
-       placeholder= 'e.g. John Doe'
-       onChangeText= {nameChangeHandler}
-       />
-       <Text>My age is...</Text>
-       <TextInput
-       style={styles.inputContainer}
-       multiline
-       keyboardType='numeric'
-       placeholder= 'e.g. 16'
-       onChangeText= {ageChangeHandler}
-       />
-      </View>
+      <Text> This is working... </Text>
     </View>
   );
 }
@@ -40,9 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  inputContainer: {
-    borderWidth: 1,
-    borderColor: '#7777',
   },
 });
